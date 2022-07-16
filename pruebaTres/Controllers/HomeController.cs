338f4bbaecd662ff8877fa89477e9cdaabd46d0c
Victor Carballo <vicarballo03@gmail.com>
2022-07-16 15:29:04 -0600

    added the project

---
+++ pruebaTres/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using pruebaTres.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pruebaTres.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult getPersons()
+        {
+            Person personas = new Person();
+            //List<Part> parts = new List<Part>();
+            List <Person> persons = personas.GetPersons();
+            persons = persons.Where(h => h.Age > 18).ToList();
+            return View(persons);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ pruebaTres/Models/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pruebaTres.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public List<Person> GetPersons()
+        {
+            List<Person> persons = new List<Person>()
+{
+new Person
+{
+Id = 1,
+FirstName = "Michael", LastName = "Pitt",
+Age = 32,
+Country = "Germany"
+},
+new Person
+{
+Id = 2,
+FirstName = "Sabrina", LastName = "Morales", Age = 6,
+Country = "Ecuador"
+},
+new Person
+{
+Id = 3,
+FirstName = "Rodrigo", LastName = "Vela",
+Age = 54,
+Country = "Peru"
+},
+new Person
+{
+Id = 4,
+FirstName = "Lorena", LastName = "Perez", Age = 18,
+Country = "Bolivia"
+},
+new Person
+{
+Id = 5,
+FirstName = "Pablo", LastName = "Picasso", Age = 19,
+Country = "Spain"
+},
+new Person
+{
+Id = 6,
+FirstName = "Walter", LastName = "Misagi", Age = 17,
+Country = "Japan"
+},
+};
+            return persons;
+        }
+    }
+
+}
